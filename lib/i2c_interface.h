2caa03b7ec2e4251fbd66f06151e6a4c5110e046
Robert Keck <37019627+keck9939@users.noreply.github.com>
2025-04-03 13:10:38 -0400

    Add guard for c++

--- lib/i2c_interface.h
+++ lib/i2c_interface.h
@@ -2,8 +2,16 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef enum { ST_OK, ST_FAIL } STAT;
 
 STAT i2c_init();
 STAT i2c_write(uint8_t addr, uint8_t* buffer, unsigned len);
 STAT i2c_deinit();
+
+#ifdef __cplusplus
+}
+#endif
